6d300ef3aea4c8a9cd1b29905942124f977991fa
kushevada <dasha.kusheva18@mail.ru>
2025-03-18 21:12:44 +0300

    [fix]

--- recsys/modulerec/views.py
+++ recsys/modulerec/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def catalog(request):
+    return render(request, 'catalog.html')
--- recsys/recsys/urls.py
+++ recsys/recsys/urls.py
@@ -22,5 +22,6 @@ from modulerec import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'), 
+    path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog')
 ]
