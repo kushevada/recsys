bc7674d2c6409ac174895e01851f4a524d689bef
kushevada <dasha.kusheva18@mail.ru>
2025-03-25 14:31:19 +0300

    [fix]

--- recsys/static/utils/catalog.js
+++ recsys/static/utils/catalog.js
@@ -1,37 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.catalog-navbar button');
+    const contentBlocks = document.querySelectorAll('.content-block');
+    const activebuttons = document.querySelectorAll('.product-button');
 
-});
+    function switchContent(targetId) {
+        contentBlocks.forEach(block => block.classList.remove('active'));
 
-const buttons = document.querySelectorAll('.catalog-navbar button');
-const contentBlocks = document.querySelectorAll('.content-block');
+        const targetBlock = document.querySelector(`${targetId}`);
+        if (targetBlock) {
+            targetBlock.classList.add('active');
+        }
+    }
 
-function switchContent(targetId) {
+    buttons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            let targetId = button.getAttribute('data-target');
+            switchContent(targetId);
+        });
+    });
 
-};
-
-buttons.forEach(button => button.addEventListener('click', function(event) {
-
-}));
-
-document.addEventListener("DOMContentLoaded", () =>{
-
-   const buttons = document.querySelectorAll(".catalog-navbar button");
-   const contentBlocks = document.querySelectorAll(".content-block");
-
-   function switchContent(targetId){
-       contentBlocks.forEach(block => block.classList.remove("active"));
-
-       const targetBlock = document.querySelector(`${targetId}`);
-       if (targetBlock){
-           targetBlock.classList.add("active");
-       }
-   }
-
-   buttons.forEach(button =>{
-      button.addEventListener("click", event =>{
-         let targetId=button.getAttribute("data-target");
-         switchContent(targetId);
-      });
-   
-});
+    activebuttons.forEach(button => {
+        button.addEventListener('click', () => {
+            activebuttons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+        });
+    });
 });
