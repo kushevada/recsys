ba9b234558c37cd28b595edc57241d994bbf8594
kushevada <dasha.kusheva18@mail.ru>
2025-03-31 03:40:18 +0300

    [fix & new]

--- recsys/modulerec/models.py
+++ recsys/modulerec/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    is_for_weight_gain = models.BooleanField(default=False)
+    is_for_weight_loss = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
