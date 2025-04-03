59071e5d35777532b7a9f714bdb800695f50fb03
karrrtavy <ignogamer90@gmail.com>
2025-04-03 14:48:07 +0300

    [module data update]

--- recsys/modulerec/apps.py
+++ recsys/modulerec/apps.py
@@ -1,6 +1,50 @@
 from django.apps import AppConfig
 
 
+
 class RecConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'modulerec'
+
+
+
+def Data(**kwargs):
+    from modulerec.models import Category, Product
+
+    categories = (
+        'meat',
+        'fish',
+        'eggs',
+        'milk',
+        'vegetables',
+        'fruits',
+        'fats',
+        'cereals',
+        'baking',
+        'drinks',
+    )
+
+    for category_name in categories:
+        category, created  = Category.objects.get_or_create(name=category_name)
+        if category.name == 'meat':
+            Product.objects.get_or_create(name='Корейка свиная запеченная',category=category, protein = 27.23, fats = 13.62, carbohydrates = 0, caloricity = 239.0)
+            Product.objects.get_or_create(name='Филе свиное приготовленное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Говядина приготовленная',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Телятина приготовленная',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Куриная грудка запеченная',category=category, protein = 0, fats = 0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Куриные ножки запеченные',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Курица жареная',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Голень индейки запеченная',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Грудка индейки вареная',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+        elif category.name == 'fish':
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
+            Product.objects.get_or_create(name='Бедро индейки запеченное',category=category, protein = 0.0, fats = 0.0, carbohydrates = 0)
--- recsys/modulerec/models.py
+++ recsys/modulerec/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class ProductCategory(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -10,9 +10,16 @@ class ProductCategory(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    proteins = models.FloatField(default=0)
+    fats = models.FloatField(default=0)
+    carbohydrates = models.FloatField(default=0)
+    caloricity = models.FloatField(default=0)
     is_for_weight_gain = models.BooleanField(default=False)
     is_for_weight_loss = models.BooleanField(default=False)
 
+
+
     def __str__(self):
         return self.name
+
