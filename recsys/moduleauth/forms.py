2d41f196d339a024171647a9c608268da71b2e65
karrrtavy <ignogamer90@gmail.com>
2025-04-04 13:08:09 +0300

    [fix]

--- recsys/moduleauth/forms.py
+++ recsys/moduleauth/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
+from django.core.exceptions import ValidationError
 
 
 # Create your forms here.
@@ -31,3 +32,20 @@ class UserRegistrationForm(forms.ModelForm):
             user.save()
             Profile.objects.get_or_create(user=user)
         return user
+    
+class UserInfoForm(forms.ModelForm):
+    height = forms.IntegerField(label='Рост', required=True)
+    weight = forms.IntegerField(label='Вес', required=True)
+
+    class Meta:
+        model = Profile
+        fields = ('height', 'weight')
+        widgets = {
+            'height': forms.NumberInput(attrs={'length': 3, 'minvalue': 120, 'maxvalue': 300}), 
+            'weight': forms.NumberInput(attrs={'minlength': 2, 'maxlength': 3, 'minvalue': 25, 'maxvalue': 500})
+        }
+
+    def ErrorMessage(self):
+        height = self.cleaned_data.get('height')
+        if height < 120 and height > 300:
+            raise forms.ValidationError('Некорректные данные')
--- recsys/moduleauth/models.py
+++ recsys/moduleauth/models.py
@@ -5,6 +5,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, related_name='profile')
+    height = models.PositiveIntegerField(User, null=False, blank=True, verbose_name='Рост', min_value=120, max_value=300, length=3)
+    weight = models.PositiveIntegerField(User, null=False, blank=True, verbose_name='Вес', min_value=25, max_value=500, min_length=2, max_length=3)
+
+def UpdateInfo(self, *args, **kwargs):
+    if self.pk:
+        updated = Profile.objects.get(pk=self.pk)
+        
+    super().save(*args, **kwargs)
 
 def __str__(self):
     return f"{self.user}"
